c7a9aee8477d6063d93c4b465afef3cbeef4c1e1
almeidx <almeidx@pm.me>
2020-10-12 22:34:52 +0100

    removed redirects

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,32 +4,11 @@ import {
 } from 'react-router-dom';
 
 import Layout from './components/Layout';
-import RedirectComponent from './components/Redirect';
 
 const Router: FC = () => (
   <BrowserRouter>
     <Switch>
       <Route component={Layout} path='/' exact />
-      <Route
-        component={() => <RedirectComponent url='https://arcanebot.xyz/leaderboard/pepeemoji' />}
-        path='/leaderboard'
-      />
-      <Route
-        component={() => <RedirectComponent url='https://teespring.com/stores/pepe-emoji-server' />}
-        path='/merch'
-      />
-      <Route
-        component={() => <RedirectComponent url='https://discord.gg/pepe' />}
-        path='/invite'
-      />
-      <Route
-        component={() => (
-          <RedirectComponent
-            url='https://discord.com/api/oauth2/authorize?client_id=506186003816513538&permissions=1916136703&scope=bot'
-          />
-        )}
-        path='/bot-invite'
-      />
       <Redirect to='/' />
     </Switch>
   </BrowserRouter>
