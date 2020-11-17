c4c226f9af1ec0f3cdb30ce7d0028bdc2894423e
almeidx <almeidx@pm.me>
2020-11-17 16:00:47 +0000

    added typings for react-animated-number

---
+++ types.d.ts
@@ -0,0 +1,17 @@
+declare module 'react-animated-number' {
+  import * as React from 'react';
+
+  export interface AnimatedNumberProps {
+    value: number;
+    initialValue?: number;
+    component?: any;
+    formatValue?: (n: number) => string;
+    duration?: number;
+    frameStyle?: (perc: number) => Object | void;
+    stepPrecision?: number;
+    className?: string;
+    style: any;
+  }
+
+  export default class AnimatedNumber extends React.Component<AnimatedNumberProps> {}
+}
