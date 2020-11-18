f3772ce288796dddb3966c9775445e4238a1052c
almeidx <almeidx@pm.me>
2020-11-18 14:09:01 +0000

    started with the commands page

---
+++ pages/bot/commands.tsx
@@ -0,0 +1,31 @@
+//@ts-ignore
+import styles from '../../styles/Commands.module.css';
+
+export default function Commands() {
+  return (
+    <div className={styles['container']} >
+      <h1>Pepe Manager Commands</h1>
+
+      <section className={styles['commands-container']} >
+        <div>
+          <h1>p!ping</h1>
+          <span>Checks the bots ping to the Discord servers.</span>
+          <p className={styles['aliases']} >Aliases: ping, pong</p>
+          {/* <p className={styles['permissions']} ></p> */}
+        </div>
+        <div>
+          <h1>p!help</h1>
+          <span>Displays a list of available commands, or detailed information for a specified command.</span>
+          <p className={styles['aliases']} >Aliases: h, commands</p>
+          <p className={styles['permissions']} >Permissions: Embed Links</p>
+        </div>
+        <div>
+          <h1>p!stats</h1>
+          <span>Displays the bots information.</span>
+          <p className={styles['aliases']} >Aliases: botinfo, bi</p>
+          <p className={styles['permissions']} >Permissions: Embed Links</p>
+        </div>
+      </section>
+    </div>
+  );
+}
--- pages/bot/index.tsx
+++ pages/bot/index.tsx
@@ -5,7 +5,8 @@ import { MdLibraryAdd, MdLayers } from 'react-icons/md';
 import { RiShieldStarFill } from 'react-icons/ri';
 import { FaDiscord } from 'react-icons/fa';
 
-import styles from '../styles/Bot.module.css';
+//@ts-ignore
+import styles from '../../styles/Bot.module.css';
 
 export default function Bot() {
   return (
