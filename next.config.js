cf5f26497723adae109fa6cd1f15dce9635aefef
almeidx <almeidx@pm.me>
2020-11-22 17:25:35 +0000

    added redirects for bot invite and vote

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  redirects() {
+    return [
+      {
+        source: '/bot-invite',
+        destination: 'https://discord.com/oauth2/authorize?client_id=506186003816513538&permissions=1611000960&scope=bot',
+        permanent: true,
+      },
+      {
+        source: '/bot-vote',
+        destination: 'https://top.gg/bot/506186003816513538/vote',
+        permanent: true,
+      },
+    ];
+  },
+};
