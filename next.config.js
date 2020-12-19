3f924a1ebb08dd1fed47d095cc4f8d843a2cd280
almeidx <almeidx@pm.me>
2020-12-19 15:44:25 +0000

    feat: added /patreon

--- next.config.js
+++ next.config.js
@@ -2,19 +2,24 @@ module.exports = {
   redirects() {
     return [
       {
-        source: '/bot-invite',
         destination: 'https://discord.com/oauth2/authorize?client_id=506186003816513538&permissions=1611000960&scope=bot',
         permanent: true,
+        source: '/bot-invite',
       },
       {
-        source: '/vote',
         destination: 'https://top.gg/bot/506186003816513538/vote',
         permanent: true,
+        source: '/vote',
       },
       {
-        source: '/merch',
         destination: 'https://teespring.com/stores/pepe-emoji-server',
         permanent: true,
+        source: '/merch',
+      },
+      {
+        destination: 'https://patreon.com/pepemanager',
+        permanent: true,
+        source: '/patreon',
       },
     ];
   },
