332dcf6fa5d17bdb0f96417b9969959677f81c99
almeidx <almeidx@pm.me>
2021-03-10 22:47:18 +0000

    feat: added custom scrollbar

--- next.config.js
+++ next.config.js
@@ -1,32 +1,30 @@
-module.exports = {
-  redirects() {
-    return [
-      {
-        destination:
-          'https://discord.com/oauth2/authorize?client_id=506186003816513538&permissions=1611000960&scope=bot',
-        permanent: true,
-        source: '/bot-invite',
-      },
-      {
-        destination: 'https://top.gg/bot/506186003816513538/vote',
-        permanent: true,
-        source: '/vote',
-      },
-      {
-        destination: 'https://teespring.com/stores/pepe-emoji-server',
-        permanent: true,
-        source: '/merch',
-      },
-      {
-        destination: 'https://patreon.com/pepemanager',
-        permanent: true,
-        source: '/patreon',
-      },
-      {
-        destination: 'https://docs.pepe-is.life/',
-        permanent: true,
-        source: '/docs',
-      },
-    ];
-  },
+exports.redirects = () => {
+  return [
+    {
+      destination:
+        'https://discord.com/oauth2/authorize?client_id=506186003816513538&permissions=1611000960&scope=bot',
+      permanent: true,
+      source: '/bot-invite',
+    },
+    {
+      destination: 'https://top.gg/bot/506186003816513538/vote',
+      permanent: true,
+      source: '/vote',
+    },
+    {
+      destination: 'https://teespring.com/stores/pepe-emoji-server',
+      permanent: true,
+      source: '/merch',
+    },
+    {
+      destination: 'https://patreon.com/pepemanager',
+      permanent: true,
+      source: '/patreon',
+    },
+    {
+      destination: 'https://docs.pepe-is.life/',
+      permanent: true,
+      source: '/docs',
+    },
+  ];
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,28 @@
 import type { AppProps } from 'next/dist/next-server/lib/router/router';
 import Navbar from '../components/Navbar';
 import SearchBarProvider from '../contexts/SearchBarContext';
+import { Scrollbars } from 'react-custom-scrollbars';
+import Head from 'next/head';
 import '../styles/global.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Scrollbars
+      autoHide
+      autoHideTimeout={1000}
+      autoHideDuration={200}
+      universal
+      style={{ width: '100vw', height: '100vh' }}
+      renderThumbVertical={({ style, ...props }) => <div {...props} style={{ ...style, background: 'var(--black)' }} />}
+    >
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+      </Head>
+
       <Navbar />
       <SearchBarProvider>
         <Component {...pageProps} />
       </SearchBarProvider>
-    </>
+    </Scrollbars>
   );
 }
