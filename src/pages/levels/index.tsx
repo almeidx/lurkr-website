073d80260cba0462ee5ef8c9845192864de73ae9
almeidx <almeidx@pm.me>
2021-03-11 11:12:55 +0000

    added base levels page

---
+++ src/pages/levels/index.tsx
@@ -0,0 +1,52 @@
+import { useRouter } from 'next/router';
+import { ChangeEvent, useEffect, useState } from 'react';
+import styles from '../../styles/pages/Levels.module.css';
+import { isValidSnowflake } from '../../utils/utils';
+import { BsArrowReturnLeft } from 'react-icons/bs';
+
+export default function Levels() {
+  const [server, setServer] = useState('');
+  const [arrowColour, setArrowColour] = useState('var(--white)');
+  const router = useRouter();
+
+  const handleSubmit = () => {
+    if (!server || !isValidSnowflake(server)) return;
+    void router.push(`/levels/${server}`);
+  };
+
+  function handleInputChange({ target }: ChangeEvent<HTMLInputElement>) {
+    setServer(target.value);
+
+    if (isValidSnowflake(target.value)) setArrowColour('green');
+    else setArrowColour('red');
+  }
+
+  useEffect(() => {
+    const enterLogic = ({ code }: KeyboardEvent) => {
+      if (code === 'Enter' || code === 'NumpadEnter') handleSubmit();
+    };
+
+    document.addEventListener('keydown', enterLogic);
+
+    return () => document.removeEventListener('keydown', enterLogic);
+  });
+
+  return (
+    <div className={styles.container}>
+      <label htmlFor="input">Leaderboard</label>
+      <div>
+        <input
+          type="number"
+          id="input"
+          value={server}
+          onChange={handleInputChange}
+          placeholder="Input a server ID to view it's leaderboard"
+          autoComplete="off"
+        />
+        <div onClick={handleSubmit}>
+          <BsArrowReturnLeft fill={arrowColour} size={100} />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -17,3 +17,9 @@ export const DISCORD_GUILD_CDN = (id: Snowflake, icon: string) =>
   `https://cdn.discordapp.com/icons/${id}/${icon}.${icon.startsWith('a_') ? 'gif' : 'webp'}?size=128`;
 
 export const API_BASE_URL = 'https://api.pepe-is.life' as const;
+
+// https://discord.com/developers/docs/reference#snowflakes
+export const MIN_SNOWFLAKE = 0b000000000000000000000000000000000000000000_00001_00001_000000000001;
+
+// 0b111111111111111111111111111111111111111111_11111_11111_111111111111 without _ which BigInt doesn't support
+export const MAX_SNOWFLAKE = BigInt('0b1111111111111111111111111111111111111111111111111111111111111111');
