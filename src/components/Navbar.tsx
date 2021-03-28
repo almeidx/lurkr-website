1ba22556f5459a2f88adbdb0a0f2e127ebefefcb
almeidx <almeidx@pm.me>
2021-03-28 18:20:38 +0100

    moved navbar into a component and improved design while signed in

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,48 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { signIn, signOut, useSession } from 'next-auth/client';
+import { FaDiscord } from 'react-icons/fa';
+
+import styles from '../styles/components/Navbar.module.css';
+
+export default function Navbar() {
+  const router = useRouter();
+  const [session] = useSession();
+
+  return (
+    <nav className={styles.navbarContainer}>
+      <span onClick={() => router.push('/')}>PEPE MANAGER</span>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/bot">Bot</Link>
+        </li>
+        <li>
+          <Link href="/levels">Levels</Link>
+        </li>
+        <li>
+          <Link href="/merch">Merch</Link>
+        </li>
+        <li>
+          <Link href="https://discord.gg/pepe">Support Server</Link>
+        </li>
+      </ul>
+      <div>
+        {session ? (
+          <button className={styles.signOutButton} onClick={() => signOut({ callbackUrl: window.location.href })}>
+            {session.user.image && (
+              <img width={'1rem'} height={'1rem'} src={session.user.image} alt="Your profile picture" />
+            )}
+            {session.user.name} - Sign out
+          </button>
+        ) : (
+          <button className={styles.signInButton} onClick={() => signIn('discord')}>
+            <FaDiscord /> Sign in
+          </button>
+        )}
+      </div>
+    </nav>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,14 @@ import '../styles/global.css';
 
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { Provider as AuthProvider, signIn, signOut, useSession } from 'next-auth/client';
+import { Provider as AuthProvider } from 'next-auth/client';
 import { Scrollbars } from 'react-custom-scrollbars';
 
+import Navbar from '../components/Navbar';
 import SearchBarProvider from '../contexts/SearchBarContext';
 import UserProvider from '../contexts/UserContext';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const [session] = useSession();
-
   return (
     <Scrollbars
       autoHide
@@ -28,35 +24,8 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
       </Head>
 
-      <nav className="navbarContainer">
-        <span onClick={() => router.push('/')}>PEPE MANAGER</span>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/bot">Bot</Link>
-          </li>
-          <li>
-            <Link href="/levels">Levels</Link>
-          </li>
-          <li>
-            <Link href="/merch">Merch</Link>
-          </li>
-          <li>
-            <Link href="https://discord.gg/pepe">Support Server</Link>
-          </li>
-        </ul>
-        <div>
-          {session ? (
-            <button onClick={() => signOut({ callbackUrl: window.location.href })}>
-              {session.user.name} - Sign out
-            </button>
-          ) : (
-            <button onClick={() => signIn('discord')}>Sign in</button>
-          )}
-        </div>
-      </nav>
+      <Navbar />
+
       <SearchBarProvider>
         <AuthProvider session={pageProps.session}>
           <UserProvider>
