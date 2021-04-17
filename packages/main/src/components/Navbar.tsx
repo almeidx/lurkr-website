266042c1abd25995c72ace229d3a3d9509426317
Almeida <almeidx@pm.me>
2021-04-17 16:11:06 +0100

    feat(main): simplified navbar

--- packages/main/src/components/Navbar.tsx
+++ packages/main/src/components/Navbar.tsx
@@ -8,13 +8,13 @@ export default function Navbar() {
 
   return (
     <nav className={styles.navbarContainer}>
-      <span onClick={() => router.push('/')}>PEPE MANAGER</span>
+      <span onClick={() => router.push('/')}>PEPE EMOJIS</span>
       <ul className={styles.linksContainer}>
         <li>
           <Link href="/">Home</Link>
         </li>
         <li>
-          <Link href="https://discord.gg/pepe">Support Server</Link>
+          <Link href="https://pepemanager.com">Pepe Manager</Link>
         </li>
         <li>
           <Link href="/privacy">Privacy</Link>
@@ -23,8 +23,6 @@ export default function Navbar() {
           <Link href="/terms">Terms</Link>
         </li>
       </ul>
-
-      <div />
     </nav>
   );
 }
