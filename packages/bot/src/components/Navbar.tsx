4ddbd5b09af28889fdc287bbc9f61d07ad8e55db
Almeida <almeidx@pm.me>
2021-04-17 16:55:38 +0100

    refactor(bot): changed icons and navbar

--- packages/bot/src/components/Navbar.tsx
+++ packages/bot/src/components/Navbar.tsx
@@ -7,6 +7,46 @@ import { UserContext } from '../contexts/UserContext';
 import styles from '../styles/components/Navbar.module.css';
 import { API_BASE_URL, DISCORD_USER_AVATAR_CDN } from '../utils/constants';
 
+// const popup = (url: string, title: string, w: number, h: number) => {
+//   // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+//   const dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : window.screenX;
+//   // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+//   const dualScreenTop = window.screenTop !== undefined ? window.screenTop : window.screenY;
+
+//   const width = window.innerWidth
+//     ? window.innerWidth
+//     : document.documentElement.clientWidth
+//     ? document.documentElement.clientWidth
+//     : screen.width;
+
+//   const height = window.innerHeight
+//     ? window.innerHeight
+//     : document.documentElement.clientHeight
+//     ? document.documentElement.clientHeight
+//     : screen.height;
+
+//   const systemZoom = width / window.screen.availWidth;
+
+//   const left = (width - w) / 2 / systemZoom + dualScreenLeft;
+//   const top = (height - h) / 2 / systemZoom + dualScreenTop;
+
+//   const newWindow = window.open(
+//     url,
+//     title,
+//     `
+//     scrollbars=yes,
+//     width=${w / systemZoom},
+//     height=${h / systemZoom},
+//     top=${top},
+//     left=${left}
+//     menubar=0,
+//     toolbar=0
+//     `,
+//   );
+
+//   newWindow?.focus();
+// };
+
 export default function Navbar() {
   const router = useRouter();
   const { authenticated, avatar, id, username, discriminator } = useContext(UserContext);
@@ -30,6 +70,9 @@ export default function Navbar() {
           <Link href="https://discord.gg/pepe">Support Server</Link>
         </li>
         <li>
+          <Link href="https://pepe-is.life">Pepe Emojis</Link>
+        </li>
+        <li>
           <Link href="/privacy">Privacy</Link>
         </li>
         <li>
@@ -42,8 +85,8 @@ export default function Navbar() {
           <button className={styles.signOutButton} onClick={() => window.open(`${API_BASE_URL}/auth/logout`, '_self')}>
             {avatar && (
               <img
-                width={'1rem'}
-                height={'1rem'}
+                width={'30px'}
+                height={'30px'}
                 src={DISCORD_USER_AVATAR_CDN(id, avatar)}
                 alt="Your profile picture"
               />
--- packages/bot/src/pages/_document.tsx
+++ packages/bot/src/pages/_document.tsx
@@ -2,132 +2,132 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 const appleIcons: { href: string; media: string }[] = [
   {
-    href: '/icons/apple-splash-2048-2732.png',
+    href: 'icons/apple-splash-2048-2732.png',
     media:
       '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
   },
   {
-    href: '/icons/apple-splash-2732-2048.png',
+    href: 'icons/apple-splash-2732-2048.png',
     media:
       '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)',
   },
   {
-    href: '/icons/apple-splash-1668-2388.png',
+    href: 'icons/apple-splash-1668-2388.png',
     media:
       '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
   },
   {
-    href: '/icons/apple-splash-2388-1668.png',
+    href: 'icons/apple-splash-2388-1668.png',
     media:
       '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)',
   },
   {
-    href: '/icons/apple-splash-1536-2048.png',
+    href: 'icons/apple-splash-1536-2048.png',
     media:
       '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
   },
   {
-    href: '/icons/apple-splash-2048-1536.png',
+    href: 'icons/apple-splash-2048-1536.png',
     media:
       '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)',
   },
   {
-    href: '/icons/apple-splash-1668-2224.png',
+    href: 'icons/apple-splash-1668-2224.png',
     media:
       '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
   },
   {
-    href: '/icons/apple-splash-2224-1668.png',
+    href: 'icons/apple-splash-2224-1668.png',
     media:
       '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)',
   },
   {
-    href: '/icons/apple-splash-1620-2160.png',
+    href: 'icons/apple-splash-1620-2160.png',
     media:
       '(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
   },
   {
-    href: '/icons/apple-splash-2160-1620.png',
+    href: 'icons/apple-splash-2160-1620.png',
     media:
       '(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)',
   },
   {
-    href: '/icons/apple-splash-1284-2778.png',
+    href: 'icons/apple-splash-1284-2778.png',
     media:
       '(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
   },
   {
-    href: '/icons/apple-splash-2778-1284.png',
+    href: 'icons/apple-splash-2778-1284.png',
     media:
       '(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)',
   },
   {
-    href: '/icons/apple-splash-1170-2532.png',
+    href: 'icons/apple-splash-1170-2532.png',
     media:
       '(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
   },
   {
-    href: '/icons/apple-splash-2532-1170.png',
+    href: 'icons/apple-splash-2532-1170.png',
     media:
       '(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)',
   },
   {
-    href: '/icons/apple-splash-1125-2436.png',
+    href: 'icons/apple-splash-1125-2436.png',
     media:
       '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
   },
   {
-    href: '/icons/apple-splash-2436-1125.png',
+    href: 'icons/apple-splash-2436-1125.png',
     media:
       '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)',
   },
   {
-    href: '/icons/apple-splash-1242-2688.png',
+    href: 'icons/apple-splash-1242-2688.png',
     media:
       '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
   },
   {
-    href: '/icons/apple-splash-2688-1242.png',
+    href: 'icons/apple-splash-2688-1242.png',
     media:
       '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)',
   },
   {
-    href: '/icons/apple-splash-828-1792.png',
+    href: 'icons/apple-splash-828-1792.png',
     media:
       '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
   },
   {
-    href: '/icons/apple-splash-1792-828.png',
+    href: 'icons/apple-splash-1792-828.png',
     media:
       '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)',
   },
   {
-    href: '/icons/apple-splash-1242-2208.png',
+    href: 'icons/apple-splash-1242-2208.png',
     media:
       '(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
   },
   {
-    href: '/icons/apple-splash-2208-1242.png',
+    href: 'icons/apple-splash-2208-1242.png',
     media:
       '(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)',
   },
   {
-    href: '/icons/apple-splash-750-1334.png',
+    href: 'icons/apple-splash-750-1334.png',
     media:
       '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
   },
   {
-    href: '/icons/apple-splash-1334-750.png',
+    href: 'icons/apple-splash-1334-750.png',
     media:
       '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)',
   },
   {
-    href: '/icons/apple-splash-640-1136.png',
+    href: 'icons/apple-splash-640-1136.png',
     media:
       '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
   },
   {
-    href: '/icons/apple-splash-1136-640.png',
+    href: 'icons/apple-splash-1136-640.png',
     media:
       '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)',
   },
@@ -135,22 +135,17 @@ const appleIcons: { href: string; media: string }[] = [
 
 const keywords: string[] = [
   'Pepe',
-  'Pepe Discord',
-  'Pepe Discord Server',
-  'Pepe Discord Guild',
-  'Pepe Emoji Server',
-  'Pepe Emojis',
-  'Pepe Emotes',
-  'Pepe Server Invite',
-  'Peepo Server Invite',
-  'Peepo Discord Server',
-  'Nitro',
-  'Discord Nitro',
-  'Peepo Emoji Server',
-  'Peepo Emoji Guild',
-  'Discord',
-  'Emotes',
-  'Emojis',
+  'Peepo',
+  'Pepe Manager',
+  'Pepe Manager Bot',
+  'Pepe Manager Bot Discord',
+  'Pepe Manager Discord',
+  'Pepe Manager Discord Server',
+  'Pepe Manager Discord Guild',
+  'Emoji Manager',
+  'Emoji Manager Discord',
+  'Pepe Manager Invite',
+  'Pepe Manager Bot Invite',
 ];
 
 export default class MyDocument extends Document {
@@ -170,17 +165,19 @@ export default class MyDocument extends Document {
 
           <meta name="keywords" content={keywords.join(', ')} />
 
-          <meta name="title" content="Pepe Emoji" />
-          <meta property="og:title" content="Pepe Emoji" />
-          <meta name="description" content="More than 1,400 unique Pepe emojis for you to use on Discord" />
-          <meta property="og:description" content="More than 1,400 unique Pepe emojis for you to use on Discord" />
-          <meta property="og:type" content="website" />
+          <meta name="title" content="Pepe Manager" />
+          <meta property="og:title" content="Pepe Manager" />
+          <meta
+            name="description"
+            content="A Discord bot with focus on automation, leveling, emoji management, and image manipulation"
+          />
           <meta
             property="og:description"
-            content="Pepe Emoji offers a powerful Discord bot to manage your server and unique Pepe emojis for you to use on Discord"
+            content="A Discord bot with focus on automation, leveling, emoji management, and image manipulation"
           />
+          <meta property="og:type" content="website" />
 
-          <link rel="apple-touch-icon" href="/icons/apple-icon-180.png" />
+          <link rel="apple-touch-icon" href="icons/apple-icon-180.png" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
 
           {appleIcons.map(({ href, media }, i) => (
