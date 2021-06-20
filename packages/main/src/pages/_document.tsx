274f33cc201ea4412a7cb6380795aeda0175bc71
Almeida <almeidx@pm.me>
2021-06-20 13:21:00 +0100

    fixed imports

--- packages/main/src/pages/_document.tsx
+++ packages/main/src/pages/_document.tsx
@@ -1,4 +1,5 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
 import { appleIcons, keywords } from '../utils/constants';
 
 export default class MyDocument extends Document {
