8aa5df8669401030d2e120f0b6bdc1230daebc6c
Almeida <almeidx@pm.me>
2021-06-26 22:51:10 +0100

    added error component and 500 page

--- packages/bot/src/pages/404.tsx
+++ packages/bot/src/pages/404.tsx
@@ -1,26 +1,5 @@
-import Head from 'next/head';
+import Error from '../components/Error';
 
 export default function _404() {
-  return (
-    <div
-      className="bg-discord-dark flex flex-col items-center justify-center text-center text-black"
-      style={{ height: 'calc(100vh - 4rem)' }}
-    >
-      <Head>
-        <title>Page Not Found | Pepe Manager</title>
-      </Head>
-
-      <div>
-        <h1 className="border-white border-solid border-r inline-block text-2xl my-0 ml-0 mr-5 py-2 pl-0 pr-6 text-white align-top font-bold">
-          404
-        </h1>
-
-        <div className="inline-block h-12 text-left align-middle" style={{ lineHeight: '49px' }}>
-          <h2 className="text-sm m-0 p-0 text-white" style={{ fontWeight: 'normal', lineHeight: 'inherit' }}>
-            This page could not be found.
-          </h2>
-        </div>
-      </div>
-    </div>
-  );
+  return <Error code={404} message="This page could not be found." title="Page Not Found" />;
 }
---
+++ packages/main/src/pages/500.tsx
@@ -0,0 +1,5 @@
+import Error from '../components/Error';
+
+export default function _500() {
+  return <Error code={500} message="Internal Server Error." title="Interval Server Error" />;
+}
