1da9f4d1083b48d10b070e6d503d9364eb52d635
Matheus Fabris <matheus_fabris@hotmail.com>
2021-06-28 18:17:28 -0300

    Fix Input comp layout and replace X with icon

--- packages/bot/src/components/Input.tsx
+++ packages/bot/src/components/Input.tsx
@@ -1,5 +1,6 @@
 import type { ChangeEventHandler, Ref } from 'react';
 import { IoMdSend } from 'react-icons/io';
+import { MdClear } from 'react-icons/md';
 
 interface InputProps {
   className?: string;
@@ -18,32 +19,33 @@ interface InputWithSubmitProps extends InputProps {
 
 export default function Input(props: InputProps | InputWithSubmitProps): JSX.Element {
   return (
-    <div className={`relative w-full ${props.className ?? ''}`}>
-      <input
-        autoCapitalize="off"
-        autoComplete="off"
-        autoCorrect="off"
-        className="text-white bg-discord-not-quite-black px-5 py-3 focus:outline-none rounded-md shadow w-full"
-        id={props.id}
-        maxLength={props.maxLength}
-        onChange={props.onChange}
-        placeholder={props.placeholder}
-        type="text"
-        value={props.value}
-      />
+    <div className={`flex w-full ${props.className ?? ''}`}>
+      <div className="relative flex-grow-1 w-full">
+        <input
+          autoCapitalize="off"
+          autoComplete="off"
+          autoCorrect="off"
+          className="text-white bg-discord-not-quite-black px-5 py-3 focus:outline-none rounded-md shadow w-full"
+          id={props.id}
+          maxLength={props.maxLength}
+          onChange={props.onChange}
+          placeholder={props.placeholder}
+          type="text"
+          value={props.value}
+        />
 
-      {props.value && (
-        <label
-          className="absolute right-0 min-w-max my-auto mx-4 text-2xl text-discord-red active:text-red-600 transition-colors h-full cursor-pointer"
-          onClick={props.onClear}
-        >
-          <span className="inline-block align-middle h-10 leading-10">x</span>
-        </label>
-      )}
-
+        {props.value && (
+          <label
+            className="absolute right-0 my-auto mx-4 py-3 text-2xl text-discord-red active:text-red-600 transition-colors h-full cursor-pointer"
+            onClick={props.onClear}
+          >
+            <MdClear />
+          </label>
+        )}
+      </div>
       {'onSubmit' in props && (
         <button
-          className="h-12 w-12 bg-discord-not-quite-black rounded-md flex justify-center items-center ml-3 text-white duration-150 transition-colors"
+          className="flex-shrink-0 h-12 w-12 bg-discord-not-quite-black rounded-md flex justify-center items-center ml-3 text-white duration-150 transition-colors"
           onClick={props.onSubmit}
           ref={props.submitRef}
         >
