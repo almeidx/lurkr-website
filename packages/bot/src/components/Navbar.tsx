800b165e83b2ffbde6d605b43221981a1877c9ce
Matheus Fabris <matheus_fabris@hotmail.com>
2021-06-28 22:56:15 -0300

    Navbar refactored to use pure CSS

--- packages/bot/src/components/Navbar.tsx
+++ packages/bot/src/components/Navbar.tsx
@@ -1,10 +1,9 @@
 import Link from 'next/link';
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { GoSignIn, GoSignOut } from 'react-icons/go';
 import { MdMenu } from 'react-icons/md';
 
 import { UserContext } from '../contexts/UserContext';
-import useWindowDimensions from '../hooks/useWindowDimensions';
 import { userAvatarCdn } from '../utils/cdn';
 import { API_BASE_URL } from '../utils/constants';
 
@@ -12,7 +11,7 @@ const links: { name: string; requireAuth?: boolean; url: string }[] = [
   { name: 'Home', url: '/' },
   { name: 'Dashboard', requireAuth: true, url: '/guilds' },
   { name: 'Levels', url: '/levels' },
-  { name: 'Calculator', url: '/levels/calculator' },
+  // { name: 'Calculator', url: '/levels/calculator' },
   { name: 'Tutorials', url: '/tutorials' },
   { name: 'Docs', url: '/docs' },
   { name: 'Support Server', url: 'https://discord.gg/pepe' },
@@ -20,68 +19,59 @@ const links: { name: string; requireAuth?: boolean; url: string }[] = [
 
 export default function Navbar() {
   const { authenticated, avatar, discriminator, id, username } = useContext(UserContext);
-  const [isMenuClosed, setIsMenuClosed] = useState(true);
-  const { width } = useWindowDimensions();
 
   return (
-    <header className="flex flex-row justify-between items-center py-6 px-6 md:gap-12 bg-discord-dark ">
+    <header className="flex md:items-center bg-discord-dark p-6">
       <Link href="/">
-        <a className="text-white md:text-xl ml-4 mr-8 whitespace-nowrap text-center uppercase font-bold">
-          Pepe Manager
-        </a>
+        <a className=" md:text-xl py-1 md:p-0 text-white uppercase whitespace-nowrap mr-4">Pepe Manager</a>
       </Link>
 
-      <button
-        className="inline-block md:hidden w-8 h-8 text-white p-1 content-none fill-current transition-colors duration-150 active:text-gray-400"
-        onClick={() => setIsMenuClosed(!isMenuClosed)}
-      >
-        <MdMenu className="w-full h-full" />
-      </button>
-
-      <nav
-        className={`${
-          width ? (isMenuClosed && width < 768 ? 'hidden' : 'flex') : 'flex'
-        } absolute md:relative top-16 left-0 md:top-0 z-20 flex-col md:flex-row md:gap-6 font-semibold w-full bg-discord-not-quite-black shadow-md rounded-lg md:rounded-none md:shadow-none md:bg-transparent p-6 pt-0 md:p-0 `}
-      >
-        <div className="flex flex-col md:flex-row md:items-center mt-6 md:mt-0 mr-auto gap-6">
-          {links.map(
-            ({ name, requireAuth, url }, i) =>
-              (!requireAuth || authenticated) && (
-                <Link href={url} key={i}>
-                  <a className="font-normal leading-7 text-gray-300 md:text-gray-500 hover:underline">{name}</a>
-                </Link>
-              ),
-          )}
-        </div>
-
-        {authenticated ? (
-          <button
-            className="text-white px-2 py-1 rounded-md flex bg-gray-700 md:bg-transparent flex-row justify-center items-center gap-3 duration-200 hover:bg-discord-lighter focus:outline-none mt-6 md:mt-0"
-            onClick={() => window.open(`${API_BASE_URL}/auth/logout`, '_self')}
-          >
-            {avatar && (
-              <img
-                alt="Your profile picture"
-                className="rounded-full"
-                height={30}
-                src={userAvatarCdn(id, avatar, 32)}
-                width={30}
-              />
+      <nav className="navbar-links md:w-full ml-auto z-20">
+        <button className="w-8 h-8 md:hidden text-white p-1">
+          <MdMenu className="w-full h-full" />
+        </button>
+        <nav className="navbar-links-dd hidden md:block absolute md:relative w-full mt-6 md:mt-0 left-0 bg-discord-not-quite-black md:bg-transparent">
+          <ul className="flex flex-col md:flex-row py-4 pr-4 md:p-0 gap-4 md:items-center">
+            {links.map(
+              ({ name, requireAuth, url }, i) =>
+                (!requireAuth || authenticated) && (
+                  <li>
+                    <Link href={url} key={i}>
+                      <a className="block w-full px-4 md:px-0 font-normal leading-7 text-gray-300 md:text-gray-500 hover:underline">
+                        {name}
+                      </a>
+                    </Link>
+                  </li>
+                ),
             )}
-            <span className="truncate max-w-[5rem] lg:max-w-none">
-              {username}#{discriminator}
-            </span>
-            <GoSignOut />
-          </button>
-        ) : (
-          <button
-            className="text-white px-2 py-1 bg-blurple rounded-md shadow-md flex flex-row justify-center items-center gap-2 duration-150 hover:bg-lighter-blurple focus:outline-none mt-6 md:mt-0"
-            onClick={() => window.open(`${API_BASE_URL}/auth`, '_self')}
-          >
-            Sign in
-            <GoSignIn />
-          </button>
-        )}
+            {authenticated ? (
+              <button
+                className="text-white cursor-pointer px-2 py-1 mx-2 md:mx-0 md:ml-auto rounded-md flex bg-gray-700 md:bg-transparent flex-row justify-center items-center gap-3 duration-200 hover:bg-discord-lighter focus:outline-none mt-6 md:mt-0"
+                onClick={() => window.open(`${API_BASE_URL}/auth/logout`, '_self')}
+              >
+                {avatar && (
+                  <img
+                    alt="Your profile picture"
+                    className="block rounded-full"
+                    height={30}
+                    src={userAvatarCdn(id, avatar, 32)}
+                    width={30}
+                  />
+                )}
+                {username}#{discriminator}
+                <GoSignOut />
+              </button>
+            ) : (
+              <button
+                className="text-white px-2 py-1 mx-2 md:mx-0 bg-blurple rounded-md shadow-md flex flex-row justify-center items-center gap-2 duration-150 hover:bg-lighter-blurple focus:outline-none mt-6 md:mt-0"
+                onClick={() => window.open(`${API_BASE_URL}/auth`, '_self')}
+              >
+                Sign in
+                <GoSignIn />
+              </button>
+            )}
+          </ul>
+        </nav>
       </nav>
     </header>
   );
