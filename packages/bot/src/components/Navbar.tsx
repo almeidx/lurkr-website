8c1be72e99a7c8687d84eac5bef2c162e862cd81
Matheus Fabris <13111055+SeikiMatt@users.noreply.github.com>
2021-06-29 17:34:32 -0300

    Reworked Navbar dropdown with different solution. Fixed h tag styling.

--- packages/bot/src/components/Navbar.tsx
+++ packages/bot/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { useContext } from 'react';
 import { GoSignIn, GoSignOut } from 'react-icons/go';
-import { MdMenu } from 'react-icons/md';
+import { MdClose, MdMenu } from 'react-icons/md';
 
 import { UserContext } from '../contexts/UserContext';
 import { userAvatarCdn } from '../utils/cdn';
@@ -23,20 +23,23 @@ export default function Navbar() {
   return (
     <header className="flex md:items-center bg-discord-dark p-6">
       <Link href="/">
-        <a className=" md:text-xl py-1 md:p-0 text-white uppercase whitespace-nowrap mr-4">Pepe Manager</a>
+        <a className=" md:text-xl py-1 md:p-0 text-white uppercase font-bold whitespace-nowrap mr-4">Pepe Manager</a>
       </Link>
 
       <nav className="navbar-links md:w-full ml-auto z-20">
-        <button className="w-8 h-8 md:hidden text-white p-1">
-          <MdMenu className="w-full h-full" />
-        </button>
+        <input id="navbar-hamburger" type="checkbox" className="hidden navbar-hamburger w-8 h-8" />
+        <label htmlFor="navbar-hamburger" className="block w-8 h-8 md:hidden text-white p-1 cursor-pointer">
+          <MdMenu className="navbar-hamburger-open w-full h-full" />
+          <MdClose className="navbar-hamburger-close hidden w-full h-full" />
+        </label>
+
         <nav className="navbar-links-dd hidden md:block absolute md:relative w-full mt-6 md:mt-0 left-0 bg-discord-not-quite-black md:bg-transparent">
           <ul className="flex flex-col md:flex-row py-4 pr-4 md:p-0 gap-4 md:items-center">
             {links.map(
               ({ name, requireAuth, url }, i) =>
                 (!requireAuth || authenticated) && (
-                  <li>
-                    <Link href={url} key={i}>
+                  <li key={i}>
+                    <Link href={url}>
                       <a className="block w-full px-4 md:px-0 font-normal leading-7 text-gray-300 md:text-gray-500 hover:underline">
                         {name}
                       </a>
@@ -63,7 +66,7 @@ export default function Navbar() {
               </button>
             ) : (
               <button
-                className="text-white px-2 py-1 mx-2 md:mx-0 bg-blurple rounded-md shadow-md flex flex-row justify-center items-center gap-2 duration-150 hover:bg-lighter-blurple focus:outline-none mt-6 md:mt-0"
+                className="text-white px-2 py-1 mx-2 md:ml-auto md:mx-0 bg-blurple rounded-md shadow-md flex flex-row justify-center items-center gap-2 duration-150 hover:bg-lighter-blurple focus:outline-none mt-6 md:mt-0"
                 onClick={() => window.open(`${API_BASE_URL}/auth`, '_self')}
               >
                 Sign in
