5bbadf3dd2865861bd5fd29c4066cc97c30f3c3f
Almeida <almeidx@pm.me>
2021-07-01 22:42:41 +0100

    pause video when outside of viewport

--- packages/bot/src/components/Showcase.tsx
+++ packages/bot/src/components/Showcase.tsx
@@ -1,3 +1,5 @@
+import { useCallback, useEffect, useRef } from 'react';
+
 export interface ShowcaseProps {
   align: 'left' | 'right';
   description: string;
@@ -6,6 +8,42 @@ export interface ShowcaseProps {
 }
 
 export default function Showcase({ align, description, src, title }: ShowcaseProps) {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  // https://stackoverflow.com/a/63887052/11252146
+  const makeVideoPlayable = useCallback((): void => {
+    if (!videoRef.current) return;
+
+    const x = videoRef.current.offsetLeft;
+    const y = videoRef.current.offsetTop;
+    const w = videoRef.current.offsetWidth;
+    const h = videoRef.current.offsetHeight;
+    const right = x + w; // right
+    const bottom = y + h; // bottom
+
+    const visibleX = Math.max(0, Math.min(w, window.pageXOffset + window.innerWidth - x, right - window.pageXOffset));
+    const visibleY = Math.max(0, Math.min(h, window.pageYOffset + window.innerHeight - y, bottom - window.pageYOffset));
+
+    const visible = (visibleX * visibleY) / (w * h);
+
+    if (visible > 0.8) {
+      void videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('scroll', makeVideoPlayable, false);
+    window.addEventListener('resize', makeVideoPlayable, false);
+
+    return () => {
+      window.removeEventListener('scroll', makeVideoPlayable);
+      window.removeEventListener('resize', makeVideoPlayable);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="flex flex-col lg:px-6 xl:px-48 items-center lg:grid lg:grid-cols-3 gap-6">
       <video
@@ -16,6 +54,7 @@ export default function Showcase({ align, description, src, title }: ShowcasePro
         muted
         playsInline
         src={src}
+        ref={videoRef}
       />
 
       <div
