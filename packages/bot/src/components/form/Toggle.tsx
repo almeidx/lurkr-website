86231e269fa47cb923bdc596dca4d7a7004c9617
Matheus Fabris <13111055+SeikiMatt@users.noreply.github.com>
2021-07-09 02:31:50 -0300

    Toggle checkbox component

---
+++ packages/bot/src/components/form/Toggle.tsx
@@ -0,0 +1,26 @@
+import { useCallback, useState } from 'react';
+
+interface ToggleProps {
+  initialValue: boolean;
+  id: string;
+  disabled?: boolean;
+  size: 'large' | 'small';
+  onChange: (value: boolean) => unknown;
+}
+
+export default function Toggle({ id, initialValue, disabled, size, onChange }: ToggleProps) {
+  const [checked, setChecked] = useState<boolean>(initialValue);
+
+  const handleClick = useCallback(() => {
+    if (disabled) return;
+    setChecked(!checked);
+    onChange(!checked);
+  }, [checked, onChange, disabled]);
+
+  return (
+    <label className={`switch ${size === 'small' && 'small h-6 w-12'}`} id={id}>
+      <input type="checkbox" onClick={handleClick} />
+      <span className={`slider ${size === 'small' && 'before:h-4 before:w-4'}`} />
+    </label>
+  );
+}
