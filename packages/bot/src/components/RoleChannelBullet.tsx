b698ee00c1a5c3b57b7959eda0641929c8c134ed
Matheus Fabris <13111055+SeikiMatt@users.noreply.github.com>
2021-07-14 17:25:54 -0300

    reusable RoleChannelBullet comp

---
+++ packages/bot/src/components/RoleChannelBullet.tsx
@@ -0,0 +1,35 @@
+import { DEFAULT_ROLE_COLOUR } from '../utils/constants';
+import { resolveColour } from '../utils/utils';
+
+interface RoleChannelBulletProps {
+  type: string;
+  roleColor?: number;
+  hoverX?: boolean;
+  name: string;
+}
+
+export default function RoleChannelBullet({ type, roleColor, hoverX, name, ...props }: RoleChannelBulletProps) {
+  const resolvedColor = roleColor ? resolveColour(roleColor) : DEFAULT_ROLE_COLOUR;
+
+  return (
+    <div
+      className={`${
+        type === 'role' ? 'role-bullet' : ''
+      } flex max-w-[250px] items-center h-6 z-50 border rounded-full text-xs select-none`}
+      style={{ borderColor: resolvedColor }}
+      {...props}
+    >
+      {type === 'role' && (
+        <>
+          {hoverX && <div className="role-x">&times;</div>}
+          <div className="w-3 h-3 ml-[5px] mr-[4px] rounded-full" style={{ backgroundColor: resolvedColor }} />
+        </>
+      )}
+
+      <div className={`${type === 'channel' ? 'hover:text-red-400 ' : ''}text-white leading-3 truncate pr-2 pb-[2px]`}>
+        {type === 'channel' && <span className="pl-2">#</span>}
+        {name}
+      </div>
+    </div>
+  );
+}
--- packages/bot/src/components/leaderboard/Role.tsx
+++ packages/bot/src/components/leaderboard/Role.tsx
@@ -1,7 +1,5 @@
 import type { GuildLevelRoles } from '../../graphql/queries/GuildLevels';
-import { DEFAULT_ROLE_COLOUR } from '../../utils/constants';
-import { resolveColour } from '../../utils/utils';
-
+import RoleChannelBullet from '../RoleChannelBullet';
 interface RoleProps {
   level: number;
   roles: GuildLevelRoles['roles'];
@@ -13,14 +11,7 @@ export default function Role({ level, roles }: RoleProps) {
       <span className="text-white">Level {level}</span>
       <div className="flex flex-row flex-wrap gap-1.5">
         {roles.map(({ color, id, name }) => (
-          <div
-            className={`flex max-w-[250px] items-center h-6 z-50 border rounded-full text-xs`}
-            style={{ borderColor: color ? resolveColour(color) : DEFAULT_ROLE_COLOUR }}
-            key={id}
-          >
-            <div className="w-3 h-3 ml-[5px] mr-[4px] rounded-full" style={{ backgroundColor: resolveColour(color) }} />
-            <div className={`text-white leading-3 truncate pr-2 pb-[2px]`}>{name}</div>
-          </div>
+          <RoleChannelBullet type="role" key={id} roleColor={color} name={name} />
         ))}
       </div>
     </div>
