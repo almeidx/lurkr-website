8241d3ea0ad89b34d39538eaf4c0510a1c48dd9e
almeidx <almeidx@pm.me>
2022-08-21 18:35:07 +0100

    add slash command banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { GiPartyPopper } from 'react-icons/gi';
 
 import Showcase from '../components/Showcase';
 import { showcases } from '../utils/constants';
@@ -17,6 +18,16 @@ const buttons: ButtonData[] = [
 export default function Home() {
   return (
     <div className="flex flex-col items-center bg-discord-dark">
+      <Link href="/invite">
+        <a className="mt-1 mb-5 flex items-center justify-center gap-x-2 rounded-md bg-green-700 py-2 px-3 text-base text-white duration-150 hover:bg-green-600	md:mt-2 md:text-xl">
+          <GiPartyPopper className="text-lg md:text-2xl" />
+          <div className="flex flex-col items-center justify-center gap-x-1 md:flex-row">
+            <p>Slash Commands update is live now!</p>
+            <p>Try it out!</p>
+          </div>
+        </a>
+      </Link>
+
       <Image src="/static/avatar.png" width={64} height={64} />
 
       <header className="flex flex-col items-center justify-center py-5">
