fd89933c16b40bed5ca7a1afe0a5449e74bded3e
Almeida <almeidx@pm.me>
2023-02-23 23:58:44 +0000

    fix build

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 // TODO: Use `${bigint}`?
 export type Snowflake = string;
 
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL as string;
 
 export const appleIcons: { href: string; media: string }[] = [
 	{
