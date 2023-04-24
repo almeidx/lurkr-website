f870980daaf61fe0398a5d6171bc397a6d2b40fc
almeidx <almeidx@pm.me>
2023-04-24 02:53:01 +0100

    update colours

--- src/components/Shard.tsx
+++ src/components/Shard.tsx
@@ -10,16 +10,16 @@ export default function Shard({ guilds, shardId, members, memory, ping, selected
 			type="button"
 		>
 			<div
-				className="invisible absolute -left-14 -top-32 z-50 w-40 rounded-lg bg-zinc-600 px-3 py-2 shadow-md group-hover:visible"
+				className="bg-discord-lighter invisible absolute -left-14 -top-32 z-50 w-40 rounded-lg px-3 py-2 text-white shadow-md group-hover:visible"
 				role="tooltip"
 			>
-				<p className="text-white">{guilds.toLocaleString()} guilds</p>
-				<p className="text-white">Members: {members.toLocaleString()}</p>
-				<p className="text-white">Memory: {memory} MB</p>
-				<p className="text-white">Ping: {ping} ms</p>
-				<p className="text-white">Uptime: {uptime ? ms(uptime) : -1}</p>
+				<p>{guilds.toLocaleString()} guilds</p>
+				<p>Members: {members.toLocaleString()}</p>
+				<p>Memory: {memory} MB</p>
+				<p>Ping: {ping} ms</p>
+				<p>Uptime: {uptime ? ms(uptime) : -1}</p>
 
-				<div className="absolute -bottom-1.5 left-1/2 h-3 w-3 -translate-x-1/2 rotate-45 bg-zinc-600 shadow-md" />
+				<div className="bg-discord-lighter absolute -bottom-1.5 left-1/2 h-3 w-3 -translate-x-1/2 rotate-45 shadow-md" />
 			</div>
 
 			{shardId}
