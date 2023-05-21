469b22fc700ee389087d775721f92db08e4e8293
almeidx <almeidx@pm.me>
2023-05-21 22:39:41 +0100

    remove vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import "tailwindcss/tailwind.css";
 import "~/styles/global.css";
 
-import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import { Open_Sans } from "next/font/google";
 import Head from "next/head";
@@ -10,7 +9,6 @@ import Footer from "~/components/Footer";
 import Navbar from "~/components/Navbar";
 import GuildProvider from "~/contexts/GuildContext";
 import UserProvider from "~/contexts/UserContext";
-import { inProductionEnvironment } from "~/utils/common";
 
 const openSans = Open_Sans({
 	weight: ["300", "400", "500", "600", "700"],
@@ -34,8 +32,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 					<Component {...pageProps} />
 					<Footer />
 				</div>
-
-				{inProductionEnvironment() ? <Analytics /> : null}
 			</GuildProvider>
 		</UserProvider>
 	);
