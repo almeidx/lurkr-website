df1fa6ca116efea70fe84092f3dd8a136a70a59f
Matt <13111055+SeikiMatt@users.noreply.github.com>
2024-04-22 04:52:48 -0300

    front page iteration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import logoImg from "@/assets/logo.png";
 import importImg from "@/assets/showcases/import.png";
 import levelingImg from "@/assets/showcases/leveling.png";
 import multipliersImg from "@/assets/showcases/multipliers.png";
@@ -9,8 +8,7 @@ import { ExternalLink } from "@/components/ExternalLink.tsx";
 import { Showcase } from "@/components/Showcase";
 import { formatNumber } from "@/utils/format-number.ts";
 import { makeApiRequest } from "@/utils/make-api-request.ts";
-import { AttachMoney, Brush, CloudDownload, Launch, MilitaryTech, Speed, TrendingUp } from "@mui/icons-material";
-import Image from "next/image";
+import { Launch } from "@mui/icons-material";
 import Link from "next/link";
 import { BOT_INVITE } from "../../shared-links.mjs";
 
@@ -18,20 +16,12 @@ export default async function Homepage() {
 	const { guildCount, memberCount, messageCount, uptime } = await getStats();
 
 	return (
-		<div className="flex flex-col items-center gap-10 mb-8 py-4">
-			<div className="flex flex-col items-center gap-4 xl:gap-0">
-				<div className="flex flex-col xl:flex-row gap-4 items-center xl:mr-20">
+		<div className="flex flex-col items-center">
+			<div className="flex flex-col items-center gap-8 mt-12 mb-8 xl:mt-24 xl:mb-16">
+				<div className="flex flex-col xl:flex-row gap-4 items-center">
 					<p className="text-3xl xl:text-4xl font-bold text-shadow-regular xl:whitespace-nowrap">
 						Level Up your Server.
 					</p>
-					<Image
-						alt="Lurkr logo"
-						className="hidden xl:block aspect-square size-72"
-						height={288}
-						width={288}
-						priority
-						src={logoImg}
-					/>
 					<p className="text-3xl xl:text-4xl font-bold text-shadow-regular xl:whitespace-nowrap">Finally, for Free.</p>
 				</div>
 
@@ -39,7 +29,7 @@ export default async function Homepage() {
 					<StartLevelingButton />
 
 					<Link
-						className="hidden xl:flex w-72 items-center justify-center gap-5 rounded-lg border border-white px-3 py-2 text-3xl font-bold hover:bg-white hover:text-black transition-colors"
+						className="hidden xl:flex w-72 items-center justify-center gap-5 rounded-lg border-2 border-white px-3 py-2 text-3xl font-bold hover:bg-white hover:text-black transition-colors duration-[50ms]"
 						href="/guilds"
 					>
 						Dashboard
@@ -48,101 +38,90 @@ export default async function Homepage() {
 				</div>
 			</div>
 
-			<div className="border-t border-b border-white/50 w-full flex items-center justify-center">
-				<div className="grid grid-cols-4 w-full px-8 py-2 xl:py-6 place-items-center max-w-xl xl:max-w-7xl">
-					<div className="flex flex-col xl:flex-row xl:gap-5">
-						<span className="font-extrabold text-2xl xl:text-6xl text-shadow-regular">
-							{formatNumber(guildCount, false)}
-						</span>
-						<p className="xl:place-self-end text-xs xl:text-base text-white/50">servers</p>
-					</div>
-					<div className="flex flex-col xl:flex-row xl:gap-5">
-						<span className="font-extrabold text-2xl xl:text-6xl text-shadow-regular">
-							{formatNumber(memberCount, false)}
-						</span>
-						<p className="xl:place-self-end text-xs xl:text-base text-white/50">members</p>
-					</div>
-					<div className="flex flex-col xl:flex-row xl:gap-5">
-						<span className="font-extrabold text-2xl xl:text-6xl text-shadow-regular">{formatNumber(uptime)}%</span>
-						<p className="xl:place-self-end text-xs xl:text-base text-white/50">uptime</p>
-					</div>
-					<div className="flex flex-col xl:flex-row xl:gap-5">
-						<span className="font-extrabold text-2xl xl:text-6xl text-shadow-regular">
-							{formatNumber(messageCount, false)}
-						</span>
-						<p className="xl:place-self-end text-xs xl:text-base text-white/50">messages</p>
-					</div>
+			<div className="max-w-[720px] xl:max-w-[1280px] w-full flex gap-8 justify-between px-8 py-4 xl:py-6 text-center xl:text-left">
+				<div className="flex flex-col xl:flex-row xl:gap-2">
+					<span className="font-extrabold xl:font-bold text-2xl xl:text-[3.75rem] xl:leading-[3.75rem] text-shadow-regular">
+						{formatNumber(guildCount, false)}
+					</span>
+					<p className="xl:place-self-end text-sm font-bold xl:font-normal tracking-wide xl:text-[1.25rem] xl:leading-[1.25rem] xl:mb-[0.275rem] text-white/55">
+						servers
+					</p>
+				</div>
+
+				<div className="flex flex-col xl:flex-row xl:gap-2">
+					<span className="font-extrabold xl:font-bold text-2xl xl:text-[3.75rem] xl:leading-[3.75rem] text-shadow-regular">
+						{formatNumber(memberCount, false)}
+					</span>
+					<p className="xl:place-self-end text-sm font-bold xl:font-normal tracking-wide xl:text-[1.25rem] xl:leading-[1.25rem] xl:mb-[0.275rem] text-white/55">
+						users
+					</p>
+				</div>
+
+				<div className="flex flex-col xl:flex-row xl:gap-2">
+					<span className="font-extrabold xl:font-bold text-2xl xl:text-[3.75rem] xl:leading-[3.75rem] text-shadow-regular">
+						{formatNumber(uptime)}%
+					</span>
+					<p className="xl:place-self-end text-sm font-bold xl:font-normal tracking-wide xl:text-[1.25rem] xl:leading-[1.25rem] xl:mb-[0.275rem] text-white/55">
+						uptime
+					</p>
+				</div>
+
+				<div className="flex flex-col xl:flex-row xl:gap-2">
+					<span className="font-extrabold xl:font-bold text-2xl xl:text-[3.75rem] xl:leading-[3.75rem] text-shadow-regular">
+						{formatNumber(messageCount, true)}
+					</span>
+					<p className="xl:place-self-end text-sm font-bold xl:font-normal tracking-wide xl:text-[1.25rem] xl:leading-[1.25rem] xl:mb-[0.275rem] text-white/55">
+						messages
+					</p>
 				</div>
 			</div>
 
-			<div className="flex flex-col items-center gap-2 px-6 text-center [text-wrap:balance]">
+			<div className="[text-wrap:balance] w-full">
 				<Showcase
 					index={0}
 					description="Seamlessly transition to Lurkr and never look back at unreasonable paywalls."
 					imgSrc={importImg}
 					title="Import & Never Lose Progress"
-				>
-					<CloudDownload className="text-[#60D1F6] size-9" />
-				</Showcase>
-
-				<ShowcaseSeparator />
+				/>
 
 				<Showcase
 					index={1}
 					description="Manage, Track & Reward your member's activity with deep and extensive configurations."
 					imgSrc={levelingImg}
 					title="Ultimate Leveling System"
-				>
-					<TrendingUp className="text-[#ff7077] size-9" />
-				</Showcase>
-
-				<ShowcaseSeparator />
+				/>
 
 				<Showcase
 					index={2}
 					description="Take complete control of your style & make sure that even your rank card expresses you."
 					imgSrc={rankCardsImg}
 					title="Customizable Rank Cards"
-				>
-					<Brush className="text-[#d2ffae] size-9" />
-				</Showcase>
-
-				<ShowcaseSeparator />
+				/>
 
 				<Showcase
 					index={3}
 					description="Incentivize activity with custom roles, permissions and other perks."
 					imgSrc={roleRewardsImg}
 					title="Endless Role Rewards"
-				>
-					<MilitaryTech className="text-[#ffe87c] size-9" />
-				</Showcase>
-
-				<ShowcaseSeparator />
+				/>
 
 				<Showcase
 					index={4}
 					description="Create global, channel & role multipliers to deeply customize leveling speed."
 					imgSrc={multipliersImg}
 					title="In-Depth Leveling Multipliers"
-				>
-					<Speed className="text-[#a475b5] size-9" />
-				</Showcase>
-
-				<ShowcaseSeparator />
+				/>
 
 				<Showcase
 					index={5}
 					description="We believe and focus in a fair premium model to unlock more configuration limits and not restrict standard features."
 					imgSrc={premiumImg}
 					title="Fair Pricing for All"
-				>
-					<AttachMoney className="text-[#b6fe94] size-9" />
-				</Showcase>
+				/>
 
-				<ShowcaseSeparator />
-
-				<StartLevelingButton />
+				<div className="flex justify-center flex-col pb-12 xl:pb-24 items-center mx-auto">
+					<StartLevelingButton />
+				</div>
 			</div>
 		</div>
 	);
@@ -150,11 +129,14 @@ export default async function Homepage() {
 
 function StartLevelingButton() {
 	return (
-		<ExternalLink
-			className="relative bg-gradient-radial w-72 z-[1] text-black text-center px-3 py-2 text-3xl font-bold rounded-lg before:absolute before:inset-0 before:bg-gradient-radial-hover before:z-[-1] before:transition-opacity before:duration-300 before:opacity-0 before:rounded-lg hover:before:opacity-100"
-			href={BOT_INVITE}
-		>
-			Start Leveling
+		<ExternalLink href={BOT_INVITE}>
+			<div className="bg-gradient-radial w-72 text-center p-0.5 text-3xl font-bold rounded-lg">
+				<div className="hover:bg-background rounded-md transition-all duration-[50ms]">
+					<div className="text-black hover:bg-gradient-radial hover:text-transparent hover:bg-clip-text py-2 transition-all duration-[50ms]">
+						Start Leveling
+					</div>
+				</div>
+			</div>
 		</ExternalLink>
 	);
 }
@@ -183,8 +165,4 @@ interface StatsResponse {
 	memberCount: number;
 	messageCount: number;
 	uptime: number;
-}
-
-function ShowcaseSeparator() {
-	return <div className="my-4 h-6 w-[2px] bg-gradient-radial" />;
 }
--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -3,28 +3,28 @@ import Image, { type StaticImageData } from "next/image";
 import type { PropsWithChildren } from "react";
 
 export function Showcase({
-	children,
-	title,
-	imgSrc,
-	description,
-	index,
-}: PropsWithChildren<{ title: string; imgSrc: StaticImageData; description: string; index: number }>) {
+							 title,
+							 imgSrc,
+							 description,
+							 index,
+						 }: PropsWithChildren<{
+	title: string;
+	imgSrc: StaticImageData;
+	description: string;
+	index: number;
+}>) {
 	return (
-		<div className={clsx("flex flex-col gap-2 xl:gap-8", index % 2 === 0 ? "xl:flex-row" : "xl:flex-row-reverse")}>
-			<Image className="aspect-[2/1]" alt={title} src={imgSrc} height={300} width={600} />
-
-			<div className="flex flex-col gap-2 xl:grid xl:grid-rows-3">
-				<div className="hidden xl:block" />
-
-				<div className="text-center gap-4 row-span-2 flex flex-col xl:grid xl:grid-cols-4">
-					<div className="hidden xl:flex xl:justify-end">
-						<div className="size-fit p-3 rounded-xl bg-darker">{children}</div>
-					</div>
-
-					<div className="col-span-3 flex flex-col xl:gap-4">
-						<p className="xl:place-self-start text-xl xl:text-3xl font-bold text-shadow-regular mt-3">{title}</p>
-						<p className="xl:text-left text-white/75 xl:text-xl tracking-tighter max-w-md">{description}</p>
-					</div>
+		<div className={clsx("", index % 2 === 0 ? "bg-[#99999911]" : "")}>
+			<div
+				className={clsx(
+					"flex justify-center flex-col py-16 xl:py-32 items-center max-w-[1280px] mx-auto gap-10 xl:gap-24",
+					index % 2 === 0 ? "xl:flex-row" : "xl:flex-row-reverse",
+				)}
+			>
+				<Image className="max-w-[600px] w-full px-8 xl:px-0" alt={title} src={imgSrc} height={300} width={600} />
+				<div className="flex flex-col text-center xl:text-left xl:w-1/3 px-4 xl:px-0">
+					<h2 className="text-xl xl:text-4xl font-bold tracking-wide mb-2 xl:mb-6">{title}</h2>
+					<p className="text-white/75 xl:text-xl max-w-md">{description}</p>
 				</div>
 			</div>
 		</div>
