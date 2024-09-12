7bbe0a4579c3d8af326c1f1a3ec79c8a64d5629a
almeidx <github@almeidx.dev>
2024-09-12 20:12:40 +0100

    ignore prefetches on middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,26 +2,30 @@ import { REDIRECT_TO_COOKIE, SIGN_IN_URL } from "@/utils/constants.ts";
 import { type NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-	// if (request.nextUrl.pathname.startsWith("/guilds")) {
 	return NextResponse.redirect(SIGN_IN_URL, {
 		headers: {
 			"Set-Cookie": `${REDIRECT_TO_COOKIE}=${request.nextUrl.pathname}; Path=/; HttpOnly; Max-Age=60;`,
 		},
 	});
-	// }
-
-	// return NextResponse.next();
 }
 
 export const config = {
 	matcher: [
 		{
 			source: "/guilds/:path*",
-			missing: [{ type: "cookie", key: "token" }],
+			missing: [
+				{ type: "cookie", key: "token" },
+				{ type: "header", key: "next-router-prefetch" },
+				{ type: "header", key: "purpose", value: "prefetch" },
+			],
 		},
 		{
 			source: "/levels/:entry/me",
-			missing: [{ type: "cookie", key: "token" }],
+			missing: [
+				{ type: "cookie", key: "token" },
+				{ type: "header", key: "next-router-prefetch" },
+				{ type: "header", key: "purpose", value: "prefetch" },
+			],
 		},
 	],
 };
