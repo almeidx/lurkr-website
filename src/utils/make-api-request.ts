5441b37ffed77fafa461b17816b7d04e1ea91c44
almeidx <github@almeidx.dev>
2024-10-27 11:34:52 +0000

    retry api requests on network errors

--- src/utils/make-api-request.ts
+++ src/utils/make-api-request.ts
@@ -1,27 +1,37 @@
 import { API_URL } from "@/utils/constants.ts";
+import pRetry, { AbortError } from "p-retry";
 
 export async function makeApiRequest(route: string, token: string | null | undefined, init: RequestInit = {}) {
-	if (token && !(init.headers as Record<string, string> | undefined)?.Authorization) {
-		init.headers = {
-			...init.headers,
-			Authorization: `Bearer ${token}`,
-		};
-	}
+	return pRetry(
+		async () => {
+			if (token && !(init.headers as Record<string, string> | undefined)?.Authorization) {
+				init.headers = {
+					...init.headers,
+					Authorization: `Bearer ${token}`,
+				};
+			}
 
-	const fullUrl = route.startsWith("http") ? route : `${API_URL}${route}`;
+			const fullUrl = route.startsWith("http") ? route : `${API_URL}${route}`;
 
-	try {
-		const response = await fetch(fullUrl, init);
+			const response = await fetch(fullUrl, init);
 
-		const authErrorHeader = response.headers.get("x-auth-error");
-		if (authErrorHeader === "malformed") {
-			console.error("Malformed token");
-		}
+			// Do not retry on 4xx errors
+			if (response.status >= 400 && response.status < 500) {
+				throw new AbortError(response.statusText);
+			}
 
-		return response;
-	} catch (error) {
-		console.error("Failed to make API request:", error);
+			const authErrorHeader = response.headers.get("x-auth-error");
+			if (authErrorHeader === "malformed") {
+				console.error("Malformed token");
+			}
 
-		throw new Error("Failed to make API request", { cause: error });
-	}
+			return response;
+		},
+		{
+			retries: 3,
+			onFailedAttempt: (error) => {
+				console.error(`Attempt ${error.attemptNumber} failed. There are ${error.retriesLeft} retries left.`);
+			},
+		},
+	);
 }
