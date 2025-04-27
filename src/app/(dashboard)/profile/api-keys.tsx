26c6f2924497b803655c144982dd40531d4d9644
almeidx <github@almeidx.dev>
2025-04-27 17:18:31 +0100

    fix api keys table on mobile

--- src/app/(dashboard)/profile/api-keys.tsx
+++ src/app/(dashboard)/profile/api-keys.tsx
@@ -67,62 +67,72 @@ export function ApiKeys({ guilds }: ApiKeysProps) {
 
 	return (
 		<>
-			<div className="sm:flex sm:items-center sm:justify-between sm:space-x-10">
-				<div className="space-y-4">
+			<div className="space-y-4">
+				<div className="flex items-center justify-between">
 					<h3 className="font-semibold text-xl">API Keys</h3>
 
-					<Text>
-						Overview of all registered API keys. Learn more about the API{" "}
-						<Link className="text-primary" href="/docs/api">
-							here
-						</Link>
-						.
-					</Text>
+					<CreateApiKeyDialog revalidateApiKeys={revalidateApiKeys} />
 				</div>
 
-				<CreateApiKeyDialog revalidateApiKeys={revalidateApiKeys} />
+				<Text>
+					Overview of all registered API keys. Learn more about the API{" "}
+					<Link className="text-primary" href="/docs/api">
+						here
+					</Link>
+					.
+				</Text>
 			</div>
 
 			{isLoading ? (
 				<LoadingSpinner />
 			) : data.length > 0 ? (
 				<>
-					<Table className="mt-4 bg-darker">
-						<TableHead>
-							<TableRow className="border-tremor-border border-b dark:border-dark-tremor-border">
-								<TableHeaderCell>Name</TableHeaderCell>
-								<TableHeaderCell>Permission</TableHeaderCell>
-								<TableHeaderCell>Guilds</TableHeaderCell>
-								<TableHeaderCell>Created At</TableHeaderCell>
-								<TableHeaderCell>Expires At</TableHeaderCell>
-								<TableHeaderCell>Last Used</TableHeaderCell>
-								<TableHeaderCell className="text-right">Actions</TableHeaderCell>
-							</TableRow>
-						</TableHead>
+					<div className="overflow-x-auto">
+						<div className="min-w-80">
+							<Table className="mt-4 bg-darker">
+								<TableHead>
+									<TableRow className="border-tremor-border border-b dark:border-dark-tremor-border">
+										<TableHeaderCell>Name</TableHeaderCell>
+										<TableHeaderCell>Permission</TableHeaderCell>
+										<TableHeaderCell>Guilds</TableHeaderCell>
+										<TableHeaderCell className="hidden md:table-cell">Created At</TableHeaderCell>
+										<TableHeaderCell className="hidden md:table-cell">Expires At</TableHeaderCell>
+										<TableHeaderCell className="hidden sm:table-cell">Last Used</TableHeaderCell>
+										<TableHeaderCell className="text-right">Actions</TableHeaderCell>
+									</TableRow>
+								</TableHead>
 
-						<TableBody>
-							{data.map((item, index) => (
-								<TableRow key={item.id}>
-									<TableCell className="font-medium">{item.name}</TableCell>
-									<TableCell>
-										<Badge variant={item.permission === ApiKeyPermission.Read ? "success" : "warning"}>
-											{item.permission === ApiKeyPermission.Read ? "Read" : "Read/Write"}
-										</Badge>
-									</TableCell>
-									<TableCell>{item.guildAccess.length}</TableCell>
-									<TableCell>{new Date(item.createdAt).toLocaleDateString()}</TableCell>
-									<TableCell>{item.expiresAt ? new Date(item.expiresAt).toLocaleDateString() : "Never"}</TableCell>
-									<TableCell>{item.lastUsedAt ? new Date(item.lastUsedAt).toLocaleDateString() : "Never"}</TableCell>
-									<TableCell className="text-right">
-										<TableRowActions
-											openDeleteDialog={() => handleOpenDeleteApiKeyDialog(index)}
-											openGuildAccessDialog={() => handleOpenGuildAccessDialog(index)}
-										/>
-									</TableCell>
-								</TableRow>
-							))}
-						</TableBody>
-					</Table>
+								<TableBody>
+									{data.map((item, index) => (
+										<TableRow key={item.id}>
+											<TableCell className="font-medium">{item.name}</TableCell>
+											<TableCell>
+												<Badge variant={item.permission === ApiKeyPermission.Read ? "success" : "warning"}>
+													{item.permission === ApiKeyPermission.Read ? "Read" : "Read/Write"}
+												</Badge>
+											</TableCell>
+											<TableCell>{item.guildAccess.length}</TableCell>
+											<TableCell className="hidden md:table-cell">
+												{new Date(item.createdAt).toLocaleDateString()}
+											</TableCell>
+											<TableCell className="hidden md:table-cell">
+												{item.expiresAt ? new Date(item.expiresAt).toLocaleDateString() : "Never"}
+											</TableCell>
+											<TableCell className="hidden sm:table-cell">
+												{item.lastUsedAt ? new Date(item.lastUsedAt).toLocaleDateString() : "Never"}
+											</TableCell>
+											<TableCell className="text-right">
+												<TableRowActions
+													openDeleteDialog={() => handleOpenDeleteApiKeyDialog(index)}
+													openGuildAccessDialog={() => handleOpenGuildAccessDialog(index)}
+												/>
+											</TableCell>
+										</TableRow>
+									))}
+								</TableBody>
+							</Table>
+						</div>
+					</div>
 
 					{focusedApiKeyIndex !== null && data[focusedApiKeyIndex] && (
 						<GuildAccessApiKeyDialog
