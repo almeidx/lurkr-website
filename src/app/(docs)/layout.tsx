e57c87f03a40d4616900ace71834101c578bcbc2
almeidx <github@almeidx.dev>
2025-04-30 00:56:43 +0100

    fix docs banner link on desktop
    bruh

--- src/app/(docs)/layout.tsx
+++ src/app/(docs)/layout.tsx
@@ -19,7 +19,7 @@ export default function Layout({ children }: { children: ReactNode }) {
 						forcedTheme: "dark",
 					}}
 				>
-					<Banner id="alpha-docs-banner" className="inline text-balance">
+					<Banner id="alpha-docs-banner" className="inline place-content-center text-balance">
 						Alpha documentation. To view the full documentation, go to{" "}
 						<ExternalLink className="ml-1 text-blurple" href="https://docs.lurkr.gg">
 							docs.lurkr.gg
