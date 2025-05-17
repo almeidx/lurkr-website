f55971bb24ed2b4acdd389d9fb2994be9f779ba2
almeidx <github@almeidx.dev>
2025-05-17 16:30:46 +0100

    increase On Join Roles Delay step to 1

--- src/app/(dashboard)/guilds/[guildId]/roles/02-on-join-roles-delay.tsx
+++ src/app/(dashboard)/guilds/[guildId]/roles/02-on-join-roles-delay.tsx
@@ -9,7 +9,7 @@ export function OnJoinRolesDelay({ defaultValue, premium }: OnJoinRolesDelayProp
 			id="autoRoleTimeout"
 			min={0}
 			max={getMaximumLimit("autoRoleTimeout", premium)}
-			step={0.5}
+			step={1}
 			defaultValue={defaultValueInMinutes}
 			steps={["0m", "5m", "10m", "15m", "20m", "25m", "30m"]}
 			mobileStepsToHide={[1, 2, 4, 5]}
