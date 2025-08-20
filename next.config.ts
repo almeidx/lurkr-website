8043953abbbc418894b17ceaf09178706b150119
almeidx <github@almeidx.dev>
2025-08-20 16:37:35 +0100

    move imgur images from remaining docs to repository

--- next.config.ts
+++ next.config.ts
@@ -84,10 +84,6 @@ const nextConfig = {
 				hostname: backgroundBucketDomain,
 				protocol: "https",
 			},
-			{
-				hostname: "i.imgur.com",
-				protocol: "https",
-			},
 		],
 	},
 	logging: {
