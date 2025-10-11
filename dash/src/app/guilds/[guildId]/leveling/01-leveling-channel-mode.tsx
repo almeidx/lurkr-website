98e5b8527b3e0c9df7c4b54e2e3b456e84477b61
almeidx <github@almeidx.dev>
2025-10-11 13:36:06 +0100

    fix radio groups resetting back to previous default value on save

--- dash/src/app/guilds/[guildId]/leveling/01-leveling-channel-mode.tsx
+++ dash/src/app/guilds/[guildId]/leveling/01-leveling-channel-mode.tsx
@@ -2,9 +2,12 @@ import { RadioProvider } from "@ariakit/react";
 import { Radio, RadioGroup } from "@/components/dashboard/Radio.tsx";
 import { XpChannelMode } from "@/lib/guild.ts";
 
-export function LevelingChannelMode({ defaultValue }: { readonly defaultValue?: XpChannelMode }) {
+export function LevelingChannelMode({ defaultValue }: { readonly defaultValue: XpChannelMode }) {
 	return (
-		<RadioProvider defaultValue={defaultValue}>
+		<RadioProvider
+			defaultValue={defaultValue} // Force remount when defaultValue changes
+			key={defaultValue}
+		>
 			<RadioGroup className="flex w-fit flex-col gap-4">
 				<label className="flex text-lg text-white/75 tracking-tight md:text-xl" htmlFor="xpChannelModeWhitelist">
 					<Radio id="xpChannelModeWhitelist" name="xpChannelMode" rightMargin value={XpChannelMode.Whitelist} />
--- dash/src/app/guilds/[guildId]/multipliers/12-role-multiplier-priority.tsx
+++ dash/src/app/guilds/[guildId]/multipliers/12-role-multiplier-priority.tsx
@@ -3,7 +3,10 @@ import { Radio, RadioGroup } from "@/components/dashboard/Radio.tsx";
 
 export function RoleMultiplierPriority({ defaultValue }: { readonly defaultValue: boolean }) {
 	return (
-		<RadioProvider defaultValue={defaultValue.toString()}>
+		<RadioProvider
+			defaultValue={defaultValue.toString()}
+			key={defaultValue.toString()} // Force remount when defaultValue changes
+		>
 			<RadioGroup className="flex w-fit flex-col gap-4">
 				<label className="flex text-lg text-white/75 tracking-tight md:text-xl" htmlFor="prioritiseMultiplierValue">
 					<Radio id="prioritiseMultiplierValue" name="prioritiseMultiplierRoleHierarchy" rightMargin value="false" />
