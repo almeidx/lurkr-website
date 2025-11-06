9587d3451adaffad0bf4bcbe1072ab9dfa761300
Almeida <github@almeidx.dev>
2025-11-06 20:39:07 +0000

    Update 06-xp-from-commands.tsx

--- dash/src/app/guilds/[guildId]/leveling/06-xp-from-commands.tsx
+++ dash/src/app/guilds/[guildId]/leveling/06-xp-from-commands.tsx
@@ -3,7 +3,7 @@ import { Toggle } from "@/components/Toggle.tsx";
 
 export function XpFromCommands({ defaultValue }: { readonly defaultValue: boolean }) {
 	return (
-		<div className="flex gap-4 rounded-lg">
+		<div className="flex gap-4">
 			<Text htmlFor="xpFromCommands">Allow members to gain experience from bot slash commands?</Text>
 			<Toggle id="xpFromCommands" initialValue={defaultValue} />
 		</div>
