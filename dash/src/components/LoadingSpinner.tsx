8bda70e04d4a5ba7e4f5e6aa0c1e9d072b24caba
almeidx <github@almeidx.dev>
2026-04-03 17:42:59 +0100

    perf: improve animation

--- dash/src/components/LoadingSpinner.tsx
+++ dash/src/components/LoadingSpinner.tsx
@@ -1,20 +1,14 @@
 export function LoadingSpinner({ size = 20 }: { size?: number }) {
 	return (
-		<svg
-			aria-hidden
-			className="animate-spin text-black dark:text-white"
-			fill="none"
-			height={size}
-			viewBox="0 0 24 24"
-			width={size}
-			xmlns="http://www.w3.org/2000/svg"
-		>
-			<circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
-			<path
-				className="opacity-75 dark:opacity-100"
-				d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-				fill="currentColor"
-			/>
-		</svg>
+		<div className="animate-spin text-black dark:text-white" style={{ height: size, width: size }}>
+			<svg aria-hidden fill="none" height={size} viewBox="0 0 24 24" width={size} xmlns="http://www.w3.org/2000/svg">
+				<circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
+				<path
+					className="opacity-75 dark:opacity-100"
+					d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+					fill="currentColor"
+				/>
+			</svg>
+		</div>
 	);
 }
