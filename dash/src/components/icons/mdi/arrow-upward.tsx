ba68364a5f059f9edec425b47c0f5d98e9e003b3
almeidx <github@almeidx.dev>
2026-05-30 18:36:17 +0100

    chore(dash): clean up unused md icons

--- dash/src/components/icons/mdi/arrow-upward.tsx
+++
@@ -1,9 +0,0 @@
-import type { ComponentProps } from "react";
-
-export function ArrowUpward(props: ComponentProps<"svg">) {
-	return (
-		<svg height="1em" viewBox="0 0 24 24" width="1em" xmlns="http://www.w3.org/2000/svg" {...props}>
-			<path d="M11 20V7.825l-5.6 5.6L4 12l8-8l8 8l-1.4 1.425l-5.6-5.6V20z" />
-		</svg>
-	);
-}
--- dash/src/components/icons/mdi/flag.tsx
+++
@@ -1,9 +0,0 @@
-import type { ComponentProps } from "react";
-
-export function Flag(props: ComponentProps<"svg">) {
-	return (
-		<svg height="1em" viewBox="0 0 24 24" width="1em" xmlns="http://www.w3.org/2000/svg" {...props}>
-			<path d="M5 21V4h9l.4 2H20v10h-7l-.4-2H7v7z" />
-		</svg>
-	);
-}
